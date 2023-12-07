a129bbfe1c396350afe2226d5231a985fbd6050a
JELollis <123717325+JELollis@users.noreply.github.com>
2023-12-07 11:45:09 -0500

    Update BardBot_Discord_0.0.2.py

--- BardBot_Discord_0.0.2.py
+++ BardBot_Discord_0.0.2.py
@@ -1,37 +1,75 @@
 import discord
 import google.cloud
+import requests
+import os
+import json
 
 from discord.ext import commands
-from discord_slash import SlashCommand
 from google.cloud import language_v1
 
 # Set your API keys
-google_cloud_api_key = "Insert_Your_Bard_API_Key"
-bot_token = "Insert_Your_Discord_Bot_Token"
-target_project_id = "Insert_Your_Bard_Project_ID"
-
-# Initialize the Discord bot with intents and commands extension
-intents = commands.Bot(command_prefix='$', intents=commands.Intents.all())
-slash = SlashCommand(intents, sync_commands=True)
+google_cloud_api_key_path = os.path.join(os.path.dirname(__file__), "bardbot.json")
+bot_token = "Insert_Your_Bot_Token"
+target_project_id = "Insert_Your_Project_ID"
+google_bard_api_key = "Insert_Your_Bard_API_Key"
 
 # Initialize the Google Cloud Natural Language API client
-client = language_v1.LanguageServiceClient.from_service_account_info(
-    google_cloud_api_key, target_project_id
+language_client = language_v1.LanguageServiceClient.from_service_account_file(
+    google_cloud_api_key_path
 )
 
+# Initialize the Discord bot with intents and commands extension
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='/', intents=intents)
 
-@intents.event
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {intents.user.name}')
+    print(f'We have logged in as {bot.user.name}')
 
-
-@intents.command(name='analyze_sentiment')
+@bot.command(name='analyze_sentiment')
 async def analyze_sentiment(ctx, text: str):
     document = language_v1.Document(content=text, type_=language_v1.Document.Type.PLAIN_TEXT)
-    sentiment = client.analyze_sentiment(document=document).document_sentiment
+    sentiment = language_client.analyze_sentiment(document=document).document_sentiment
 
     await ctx.send(f'Sentiment score: {sentiment.score}, Magnitude: {sentiment.magnitude}')
 
+@bot.command(name='Bard')
+async def bard_command(ctx, *, question: str):
+    # Call the Google Bard API with the provided question
+    response = get_bard_response(question)
+
+    # Send the response back to the Discord channel
+    await ctx.send(response)
+
+def get_bard_response(question):
+    # Google Bard API endpoint
+    bard_api_url = "https://language.googleapis.com/v1/documents:analyzeSentiment"
+
+    # Prepare the request payload
+    payload = {
+        "question": {
+            "text": question
+        }
+    }
+
+    # Include your Google Bard API key in the request headers
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {google_bard_api_key}"
+    }
+
+    # Make the API request
+    response = requests.post(bard_api_url, json=payload, headers=headers)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the JSON response and extract the answer
+        answer = response.json().get("answer", "No answer found.")
+        return answer
+    else:
+        # Handle errors
+        return f"Error: Unable to get answer. Status code: {response.status_code}"
 
 # Run the bot
-intents.run(bot_token)
+bot.run(bot_token)
